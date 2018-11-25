45963b80cf3f15f16df1fc651470563d14174ab0
Oriol Tauleria <oriol.tauleria@gmail.com>
2018-11-25 18:47:39 +0100

    Challenge 1 - Fix app

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from os import environ
 
 from bson import json_util
-from bson.objectid import ObjectId
-from flask import Flask, jsonify
+from bson.objectid import ObjectId, InvalidId
+from flask import Flask, jsonify, make_response
 from flask_pymongo import PyMongo
 
-from src.mongoflask  import MongoJSONEncoder, ObjectIdConverter, find_restaurants
+from src.mongoflask import MongoJSONEncoder, ObjectIdConverter, find_restaurants
 
 app = Flask(__name__)
 app.config["MONGO_URI"] = environ.get("MONGO_URI")
@@ -13,6 +13,9 @@ app.json_encoder = MongoJSONEncoder
 app.url_map.converters["objectid"] = ObjectIdConverter
 mongo = PyMongo(app)
 
+def jsonify_no_content():
+    response = make_response('', 204)
+    return response
 
 @app.route("/api/v1/restaurant")
 def restaurants():
@@ -22,8 +25,12 @@ def restaurants():
 
 @app.route("/api/v1/restaurant/<id>")
 def restaurant(id):
-    restaurants = find_restaurants(mongo, id)
-    return jsonify(restaurants)
+    try:
+        restaurant, = find_restaurants(mongo, id)
+        return jsonify(restaurant)
+    except (ValueError, InvalidId) as ex:
+        print("{}".format(ex))
+        return jsonify_no_content()
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=False, port=8080)
--- tests/test_restaurant.py
+++ tests/test_restaurant.py
@@ -3,6 +3,8 @@ from unittest import TestCase
 from bson.objectid import ObjectId
 from mock import patch
 import src.mongoflask
+import app
+import json
 
 
 def mock_find_restaurant(mongo, id=None):
@@ -37,3 +39,30 @@ class TestRestaurant(TestCase):
         data = src.mongoflask.find_restaurants(None, "55f14312c7447c3da7051b39")
         self.assertEqual(len(data), 1)
         self.assertTrue(data[0].get("type_of_food") == "Chinese")
+
+    @patch('app.find_restaurants')
+    def test_resturant_returns_a_unique_element_list(self, mock_restaurant):
+        mock_restaurant.side_effect = mock_find_restaurant
+        with app.app.app_context():
+            req = app.restaurant("55f14312c7447c3da7051b39")
+        data = req.get_json()
+        self.assertEqual(req.status_code, 200 )
+        self.assertTrue(data.get("type_of_food") == "Chinese")
+
+    @patch('app.find_restaurants')
+    def test_resturant_returns_a_204_when_objectId_is_not_valid(self, mock_restaurant):
+        print("test 1")
+        mock_restaurant.side_effect = mock_find_restaurant
+        with app.app.app_context():
+            req = app.restaurant("55b14312c7447c3da7051b39")  
+        data = req.get_json()
+        self.assertEqual(req.status_code, 204)
+
+    @patch('app.find_restaurants')
+    def test_resturant_returns_a_204_when_objectId_not_exists(self, mock_restaurant):
+        print("test 1")
+        mock_restaurant.side_effect = mock_find_restaurant
+        with app.app.app_context():
+            req = app.restaurant("55b14312c7447c3da7051b39")  
+        data = req.get_json()
+        self.assertEqual(req.status_code, 204)
